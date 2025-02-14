f267ec1303cf73dd58fe20ffa38d00a6abc7ce37
deeksha <billadeeksha1407@gmail.com>
2025-02-13 22:52:30 -0600

    added comment

--- src/main/java/com/telusko/quizapp/controller/QuizController.java
+++ src/main/java/com/telusko/quizapp/controller/QuizController.java
@@ -1,17 +1,21 @@
 package com.telusko.quizapp.controller;
 
-import com.telusko.quizapp.model.Question;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.telusko.quizapp.model.QuestionWrapper;
 import com.telusko.quizapp.model.Response;
 import com.telusko.quizapp.service.QuizService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-// controller for quiz
+// controller for quiz1
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
